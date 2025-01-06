46f44a8eb4e5cd3ee34433436931524c2ea3c390
Brandon Mathis <bemathis@gmail.com>
2025-01-06 11:38:44 -0500

    Added debugger statement to top of /api/hello function

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  debugger;
   const { name = "World" } = req.query;
 
   console.log("This is a log message from the serverless function");
